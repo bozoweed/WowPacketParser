b2af6ca85e5fa098ab9e9267608a493c4f6ac30b
bozoweed <bozoweed@gmail.com>
2016-09-19 09:52:16 +0200

    add scenes support

--- WowPacketParserModule.V6_0_2_19033/Parsers/SceneHandler.cs
+++ WowPacketParserModule.V6_0_2_19033/Parsers/SceneHandler.cs
@@ -1,6 +1,9 @@
-﻿using WowPacketParser.Enums;
+using WowPacketParser.Enums;
 using WowPacketParser.Misc;
 using WowPacketParser.Parsing;
+using WowPacketParser.Loading;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
 
 namespace WowPacketParserModule.V6_0_2_19033.Parsers
 {
